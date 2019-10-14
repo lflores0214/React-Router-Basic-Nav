fc442b05d7a79f4d7d465ec89fc5c9fb3e9db772
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2019-10-14 14:22:49 -0600

    unstaging

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,26 +1,19 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
 
 const Navigation = () => {
   return (
     <div>
       <div className="App">
         <h1>React Router Mini</h1>
-        <Link to={`/`}>
-          <div>
-            <a href="">Home</a>
-          </div>
-        </Link>
-        <Link to={`/about`}>
-          <div>
-            <a href="">About</a>
-          </div>
-        </Link>
-        <Link to={`/contact`}>
-          <div>
-            <a href="">Contact</a>
-          </div>
-        </Link>
+        <div>
+          <a href="">Home</a>
+        </div>
+        <div>
+          <a href="">About</a>
+        </div>
+        <div>
+          <a href="">Contact</a>
+        </div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter as Router } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById('root'));
